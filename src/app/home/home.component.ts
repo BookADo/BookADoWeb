771c082d3b7a12fa1253174a26d2142d68a44d4d
amallard <adam.allard@live.com>
2018-07-23 13:13:24 -0700

    fixing homepage css, again

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    document.body.className = 'home-container-background';
+  }
+  ngOnDestroy() {
+    document.body.className = '';
+  }
 
   ngOnInit() {
   }
-
 }
